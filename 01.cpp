8b8b6d358c262c900838bbddfb07cdbeec8cccfe
Subham Negi <negisubham210@gmail.com>
2026-07-31 11:37:18 +0530

    Initial commit

---
+++ 01.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout<<"Enter the 1 num:";
+    cin>>a;
+    cout<<"Enter the 2 num:";
+    cin>>b;
+    cout<<"The sum is:"<<a+b;
+    return 0;
+}
---
+++ 02Type_Casting.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a = 10;
+    char b ='c';
+    a = b;
+    cout<<a;
+    
+    return 0;
+}
