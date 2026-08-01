cdce22e910814cb73c92af02c74e7103077f3fb3
Subham Negi <negisubham210@gmail.com>
2026-08-01 17:30:30 +0530

    commit changes

---
+++ 10If_ElseIf.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char c = 'a';
+    cout<<"Enter a alphabet: ";
+    cin>>c;
+    if(c=='a'){
+        cout<<"It is vowel";
+    }
+    else if(c=='e'){
+        cout<<"It is vowel";
+    }
+    else if(c=='i'){
+        cout<<"It is vowel";
+    }
+    else if(c=='o'){
+        cout<<"It is vowel";
+    }
+    else if(c=='u'){
+        cout<<"It is vowel";
+    }
+    else{
+        cout<<"It is consonant";
+    }
+    return 0;
+}
---
+++ 11If_ElseIf2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num;
+    cout<<"Enter a valid number for day: ";
+    cin>>num;
+    if(num==1){
+        cout<<"Sunday";
+    }
+    else if(num==2){
+        cout<<"Monday";
+    }
+    else if(num==3){
+        cout<<"Tuesday";
+    }
+    else if(num==4){
+        cout<<"Wednesday";
+    }
+    else if(num==5){
+        cout<<"Thrusday";
+    }
+    else if(num==6){
+        cout<<"Friday";
+    }
+    else if(num==7){
+        cout<<"Saturday";
+    }
+    else{
+        cout<<"Invalid Number";
+    }
+    return 0;
+}
