ed467e660393e2e79631a764b6dee82f12c16deb
Subham Negi <negisubham210@gmail.com>
2026-08-03 12:00:29 +0530

    commit

---
+++ 24Fibonacci_Series.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    int first =0, second =1,last;
+    
+    for(int i=1; i<=n; i++ ){
+        cout<<first<<endl;
+        last = first + second;
+        first = second;
+        second = last;
+    }
+    return 0;
+}
