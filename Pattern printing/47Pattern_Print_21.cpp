9569ec58a3aedf22953ac9c21ea846acb931aed6
Subham Negi <negisubham210@gmail.com>
2026-08-04 20:55:43 +0530

    commit

---
+++ Pattern printing/47Pattern_Print_21.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int row, col;
+    for(row=5; row>=1; row--)
+    {
+        for(col=1; col<=5-row; col++){
+            cout<<"  ";
+        }
+        for(col=1; col<=2*row-1; col++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
