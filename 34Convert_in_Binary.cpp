5ba70b93439b8209c96869f0bf219827eacbf135
Subham Negi <negisubham210@gmail.com>
2026-08-05 22:48:30 +0530

    commit

---
+++ 34Convert_in_Binary.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num;
+    cout<<"Enter a num: ";
+    cin>>num;
+    int rem, mul=1, ans=0;
+
+    while(num>0){
+        //reminder
+        rem = num%2;
+        //quotient
+        num = num/2;
+        //ans
+        ans = rem*mul+ans;
+        //update mul
+        mul= mul*10;
+
+    }
+    cout<<ans;
+}
