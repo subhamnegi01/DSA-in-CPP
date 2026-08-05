de0c3a5fd828805b2506451328ab5262d9f90915
Subham Negi <negisubham210@gmail.com>
2026-08-05 17:19:04 +0530

    commit

---
+++ 30Natural_Nums_Sum.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int i = 1;
+    int sum = 0;
+    do{
+        sum = sum +i;
+        i++;
+    }
+
+    while(i<=10);
+    cout<<sum;
+    return 0;
+}
