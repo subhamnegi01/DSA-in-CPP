17bf112851c335c6d8e9a560746d2a30cfd8d465
Subham Negi <negisubham210@gmail.com>
2026-08-05 17:10:22 +0530

    commit

---
+++ 28Print_Odd_Num.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    int i = 1;
+    while(i<=n){
+        if(i%2!=0){
+            cout<<i<<endl;
+        }
+        i++;
+    }
+    return 0;
+}
