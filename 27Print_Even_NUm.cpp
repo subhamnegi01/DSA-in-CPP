9a90750e3311f7354df70dccf8e84e0423fc66ab
Subham Negi <negisubham210@gmail.com>
2026-08-05 17:00:53 +0530

    commit

--- 27Print_Even_NUm.cpp
+++ 27Print_Even_NUm.cpp
@@ -6,5 +6,13 @@ int main ()
     int n;
     cout<<"Enter a number: ";
     cin>>n;
-    
+
+    int i = 1;
+    while(i<=n){
+        if(i%2==0){
+            cout<<i<<endl;
+        }
+        i++;
+    }
+    return 0;
 }
