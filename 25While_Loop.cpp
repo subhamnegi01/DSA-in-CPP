22cf99e3e8be8a9cfd5090639942756a41572961
Subham Negi <negisubham210@gmail.com>
2026-08-05 16:57:07 +0530

    commit

---
+++ 25While_Loop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n = 8;
+    int i = 1;
+    while(i<=10){
+        cout<<n*i<<" ";
+        i++;
+        
+    }
+    
+    return 0;
+}
---
+++ 26Find_Factors.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    int i = 1;
+    while(i<=n){
+        if (n%i==0)
+        {
+            cout<<i<<endl;
+        }
+        i++;
+    }
+    return 0;
+}
