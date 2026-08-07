2bd777c37702aab8b189a3d48ad0161b916e5928
Subham Negi <negisubham210@gmail.com>
2026-08-07 21:21:34 +0530

    commit

--- Pattern printing/27Pattern_Print_3.cpp
+++ Pattern printing/27Pattern_Print_3.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     int n, row, col;
     cout<<"Enter a number for row and col: ";
     cin>>n;
     int count = 0;
     for(row = 1; row<=n; row++){
         for(col = 1; col<=n; col++){
-            
             cout<<col<<" ";
         }
         cout<<endl;
