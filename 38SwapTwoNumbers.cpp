a837bd09e72e9ff8b087b92814710e547d8a9e64
Subham Negi <negisubham210@gmail.com>
2026-08-08 23:03:38 +0530

    commit

--- 38SwapTwoNumbers.cpp
+++ 38SwapTwoNumbers.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-void swap(int &a , int &b)     // Pass by reference
+void Swap(int &a , int &b)     // Pass by reference
 {
     int c;
     c = a;
@@ -9,10 +9,21 @@ void swap(int &a , int &b)     // Pass by reference
     b = c;
 }
 
+void Swap(float &c , float &d)       // Function overloading --> Same name but different parameter
+{
+    float r = c;
+    c = d;
+    d =r;
+}
+
 int main(){
     int a, b;
     cin>>a>>b;
 
-    swap(a, b);
-    cout<<a<<" "<<b<<" ";
+    Swap(a, b);
+    cout<<a<<" "<<b<<" "<<endl;
+
+    float f1 = 3.4, f2 = 4.8;
+    Swap(f1, f2);
+    cout<<f1<<" "<<f2;
 }
