9f9bc5d12acaae67373b9370aa5b91729383b38b
Subham Negi <negisubham210@gmail.com>
2026-08-09 22:40:54 +0530

    commit

--- 35BinaryToDecimal.cpp
+++ 35BinaryToDecimal.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     int num;
     cout<<"Enter a Binary Num: ";
     cin>>num;
     int rem, mul = 1, ans = 0;
 
-    while(num>0){
+    while(num>0)
+    {
         rem = num%10;
         num = num/10;
         ans = rem *mul+ans;
