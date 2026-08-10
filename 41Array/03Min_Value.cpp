5363cfd85754d72dfea0d64abe40a27512010b5c
Subham Negi <negisubham210@gmail.com>
2026-08-10 22:39:06 +0530

    commit

---
+++ 41Array/03Min_Value.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr[5]= {2,2,4,1,9};
+    int ans = INT8_MAX;
+
+    for(int i = 0; i<5; i++)
+    {
+        if(arr[i]<ans)
+        ans = arr[i];
+    }
+    cout<<ans;
+    return 0;
+}
---
+++ 41Array/04Max_Value.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr[5]= {23, 1, 32,  43, 11};
+    int ans = INT8_MIN;
+
+    for (int i = 0; i<5 ; i++){
+
+        if(arr[i]>ans){
+            ans = arr[i];
+        }
+    }
+    cout<<ans;
+    return 0;
+}
