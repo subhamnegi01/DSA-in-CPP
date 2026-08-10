6064d599dbb0b42629ef61a0f365d654576e841f
Subham Negi <negisubham210@gmail.com>
2026-08-10 22:19:23 +0530

    commit

--- 41Array/01Array.cpp
+++ 41Array/01Array.cpp
@@ -2,7 +2,12 @@
 using namespace std;
 
 int main (){
-    int arr[]= {1,2,3,4,4,5,4,2,5,3,10 };
+    //int arr[]= {1,2,3,4,4,5,4,2,5,3,10 };
+
+    int arr[10];
+    for(int i = 0; i<=10; i++){
+        cin>>arr[i];
+    }
 
     for(int i= 0; i<=10; i++){
         cout<<arr[i]<<" ";
