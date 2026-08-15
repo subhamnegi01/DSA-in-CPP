080af68c5fc6a3cbd343467d652cf720be442423
Subham Negi <negisubham210@gmail.com>
2026-08-15 23:48:20 +0530

    commit

---
+++ 41Array/06Reverse_Array.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr[6]= {2, 3, 4, 5, 6, 7};
+
+    int start = 0, end = 5;
+
+    while(start<end){
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
+
+    }
+    for (int i = 0; i<6; i++){
+        cout<<arr[i]<<" ";
+    }
+}
