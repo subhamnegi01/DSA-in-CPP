f4f89eb09fbb606d586473854b7118d04675756c
Subham Negi <negisubham210@gmail.com>
2026-08-15 21:45:57 +0530

    commit

--- Pattern printing/30Pattern_Print_5.cpp
+++ Pattern printing/30Pattern_Print_5.cpp
@@ -8,9 +8,11 @@ int main()
     //cout<<"Enter a alphabet: ";
     //cin>>n;
 
-    for(row = 1; row<=5; row++){
+    for(row = 1; row<=5; row++)
+    {
         char name = 'a'+(row-1);
-        for(col=1; col<=5; col++){
+        for(col=1; col<=5; col++)
+        {
             cout<<name<<" ";
         }
         cout<<endl;
