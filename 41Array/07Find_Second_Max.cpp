a2414c34b39817d456e723a79b1b9a8ef21b4425
Subham Negi <negisubham210@gmail.com>
2026-08-16 00:27:08 +0530

    commit

---
+++ 41Array/07Find_Second_Max.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int print2largest(int arr [], int n){
+    int ans = INT_MIN;
+    for (int i = 0; i<n; i++){
+        if (arr[i]>ans){
+            ans = arr[i];
+        }
+    } 
+    int second = INT_MIN;
+    for(int i = 0; i<n; i++){
+        if(arr[i]!=ans){
+            second= max(second, arr[i]);
+        }
+    }
+    return second;
+}
+
+int main (){
+    int arr[6] = {2, 4, 5, 6 ,3, 8};
+    cout<<print2largest(arr, 6);
+
+    return 0;
+}
