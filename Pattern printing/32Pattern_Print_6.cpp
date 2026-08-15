da9a12fe2ccf47d784a5078d924b0593371f9374
Subham Negi <negisubham210@gmail.com>
2026-08-15 21:46:19 +0530

    commit

--- Pattern printing/32Pattern_Print_6.cpp
+++ Pattern printing/32Pattern_Print_6.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     int n, row, col, count=1;
     cout<<"Enter a number: ";
     cin>>n;
 
-    for(row =1; row<=n; row++){
-        for(col= 1; col<=n; col++){
+    for(row =1; row<=n; row++)
+    {
+        for(col= 1; col<=n; col++)
+        {
             cout<<count<<" ";
             count= count+1;
         }
