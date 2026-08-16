1973c1669842f1f17dca8290bd47f1e22e39e39f
Subham Negi <negisubham210@gmail.com>
2026-08-17 00:11:10 +0530

    commit

--- 41Array/10Rotate_Arr_by_1.cpp
+++ 41Array/10Rotate_Arr_by_1.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 using namespace std;
 
+void rotate(int arr[], int n){
+    int last = arr[n-1];
+
+    for (int i = n-2; i>=0; i--){
+        arr[i+1]= arr[i];
+
+    }
+    arr[0]= last;
+}
+
 int main(){
-    
+    int arr[5] = {1, 2, 3, 4, 5};
+
+    rotate(arr, 5);
+
+    for (int i = 0; i <5; i++){
+        cout<<arr[i]<<" ";
+
+    }
+    return 0;
 }
