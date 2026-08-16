f68121db5e90ed9ad2d24e550a6d461bc8d04c29
Subham Negi <negisubham210@gmail.com>
2026-08-16 23:39:10 +0530

    commit

--- 41Array/08Missing_Num.cpp
+++ 41Array/08Missing_Num.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int missingNumber(vector<int>& array, int n){
-    
+
+    int sum = 0;
+    int ans;
+    for(int i = 0; i<n-1; i++){
+        sum = sum + array[i];
+
+        ans = n*(n+1)/2;
+
+    }
+    return ans - sum;
+
 }
 
 int main(){
+    vector<int> array = {1, 2, 4, 5};
 
+    cout << missingNumber(array, 5);
+
+    return 0;
 }
