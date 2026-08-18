c34235563b2c506c3039b2d52bedeee48f84953a
Subham Negi <negisubham210@gmail.com>
2026-08-18 23:48:19 +0530

    commit

--- 41Array/07Find_Second_Max.cpp
+++ 41Array/07Find_Second_Max.cpp
@@ -8,7 +8,7 @@ int print2largest(int arr [], int n){
         if (arr[i]>ans){
             ans = arr[i];
         }
-    } 
+    }
     int second = INT_MIN;
     for(int i = 0; i<n; i++){
         if(arr[i]!=ans){
